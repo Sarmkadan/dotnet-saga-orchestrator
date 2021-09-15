53bf9493296b9f6ffa8edab2b74117d2ce590518
Vladyslav Zaiets <rutova2@gmail.com>
2021-09-15 19:50:41 +0300

    refactor: reshuffle CompensationService a bit

---
+++ src/Core/Extensions/ValidationExtensions.cs
@@ -0,0 +1,139 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace SagaOrchestrator.Core.Extensions;
+
+/// <summary>
+/// Validation extension methods for common validation patterns.
+/// Provides fluent validation API for parameter checking.
+/// </summary>
+public static class ValidationExtensions
+{
+    public static T NotNull<T>(this T? value, string paramName) where T : class
+    {
+        if (value == null)
+            throw new ArgumentNullException(paramName);
+        return value;
+    }
+
+    public static string NotNullOrEmpty(this string? value, string paramName)
+    {
+        if (string.IsNullOrEmpty(value))
+            throw new ArgumentException($"{paramName} cannot be null or empty", paramName);
+        return value;
+    }
+
+    public static string NotNullOrWhiteSpace(this string? value, string paramName)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException($"{paramName} cannot be null or whitespace", paramName);
+        return value;
+    }
+
+    public static int InRange(this int value, int min, int max, string paramName)
+    {
+        if (value < min || value > max)
+            throw new ArgumentOutOfRangeException(paramName, $"Value must be between {min} and {max}");
+        return value;
+    }
+
+    public static int GreaterThan(this int value, int min, string paramName)
+    {
+        if (value <= min)
+            throw new ArgumentOutOfRangeException(paramName, $"Value must be greater than {min}");
+        return value;
+    }
+
+    public static int GreaterThanOrEqual(this int value, int min, string paramName)
+    {
+        if (value < min)
+            throw new ArgumentOutOfRangeException(paramName, $"Value must be greater than or equal to {min}");
+        return value;
+    }
+
+    public static long GreaterThan(this long value, long min, string paramName)
+    {
+        if (value <= min)
+            throw new ArgumentOutOfRangeException(paramName, $"Value must be greater than {min}");
+        return value;
+    }
+
+    public static TimeSpan GreaterThanZero(this TimeSpan value, string paramName)
+    {
+        if (value <= TimeSpan.Zero)
+            throw new ArgumentException($"{paramName} must be greater than zero", paramName);
+        return value;
+    }
+
+    public static decimal InRange(this decimal value, decimal min, decimal max, string paramName)
+    {
+        if (value < min || value > max)
+            throw new ArgumentOutOfRangeException(paramName, $"Value must be between {min} and {max}");
+        return value;
+    }
+
+    public static IEnumerable<T> NotEmpty<T>(this IEnumerable<T>? value, string paramName)
+    {
+        if (value == null || !value.Any())
+            throw new ArgumentException($"{paramName} cannot be null or empty", paramName);
+        return value;
+    }
+
+    public static Guid NotEmpty(this Guid value, string paramName)
+    {
+        if (value == Guid.Empty)
+            throw new ArgumentException($"{paramName} cannot be empty GUID", paramName);
+        return value;
+    }
+
+    public static T? ValidateIf<T>(this T? value, Func<T, bool> validator, string message)
+    {
+        if (value != null && !validator(value))
+            throw new ArgumentException(message);
+        return value;
+    }
+
+    public static string ValidateEmail(this string value, string paramName)
+    {
+        if (!value.IsValidEmail())
+            throw new ArgumentException($"{paramName} must be a valid email address", paramName);
+        return value;
+    }
+
+    public static string ValidateUrl(this string value, string paramName)
+    {
+        if (!value.IsValidUrl())
+            throw new ArgumentException($"{paramName} must be a valid URL", paramName);
+        return value;
+    }
+
+    public static string MaxLength(this string value, int maxLength, string paramName)
+    {
+        if (value.Length > maxLength)
+            throw new ArgumentException($"{paramName} cannot exceed {maxLength} characters", paramName);
+        return value;
+    }
+
+    public static string MinLength(this string value, int minLength, string paramName)
+    {
+        if (value.Length < minLength)
+            throw new ArgumentException($"{paramName} must be at least {minLength} characters", paramName);
+        return value;
+    }
+
+    public static T[] NotEmptyArray<T>(this T[]? value, string paramName)
+    {
+        if (value == null || value.Length == 0)
+            throw new ArgumentException($"{paramName} cannot be null or empty", paramName);
+        return value;
+    }
+
+    public static Dictionary<K, V> NotEmptyDictionary<K, V>(this Dictionary<K, V>? value, string paramName) where K : notnull
+    {
+        if (value == null || value.Count == 0)
+            throw new ArgumentException($"{paramName} cannot be null or empty", paramName);
+        return value;
+    }
+}
---
+++ src/Infrastructure/BackgroundWorkers/EventProcessingWorker.cs
@@ -0,0 +1,167 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SagaOrchestrator.Infrastructure.Events;
+
+namespace SagaOrchestrator.Infrastructure.BackgroundWorkers;
+
+/// <summary>
+/// Background worker that processes and archives saga events.
+/// Periodically polls the event bus and handles event persistence and cleanup.
+/// </summary>
+public class EventProcessingWorker : BackgroundService
+{
+    private readonly IEventBus _eventBus;
+    private readonly ILogger<EventProcessingWorker> _logger;
+    private readonly TimeSpan _checkInterval = TimeSpan.FromSeconds(10);
+    private readonly int _maxEventsToKeep = 10000;
+
+    public EventProcessingWorker(
+        IEventBus eventBus,
+        ILogger<EventProcessingWorker> logger)
+    {
+        _eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Event Processing Worker started");
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                await ProcessEventsAsync(stoppingToken);
+                await Task.Delay(_checkInterval, stoppingToken);
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error in event processing worker");
+            }
+        }
+
+        _logger.LogInformation("Event Processing Worker stopped");
+    }
+
+    private async Task ProcessEventsAsync(CancellationToken stoppingToken)
+    {
+        var events = _eventBus.GetEventHistory();
+
+        if (events.Count == 0)
+            return;
+
+        // Log event statistics
+        var eventGroups = events
+            .GroupBy(e => e.EventType)
+            .ToDictionary(g => g.Key, g => g.Count());
+
+        _logger.LogDebug("Event queue status | Total events: {Count}, Types: {Types}",
+            events.Count,
+            string.Join(", ", eventGroups.Select(kvp => $"{kvp.Key}:{kvp.Value}")));
+
+        // Clean up old events if exceeding limit
+        if (events.Count > _maxEventsToKeep)
+        {
+            _logger.LogWarning("Event history exceeds limit | Current: {Count}, Max: {Max}",
+                events.Count, _maxEventsToKeep);
+            _eventBus.ClearHistory();
+        }
+
+        // Process recent events (last 100)
+        var recentEvents = events.TakeLast(100).ToList();
+        foreach (var @event in recentEvents)
+        {
+            try
+            {
+                await ProcessEventAsync(@event, stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error processing event {EventId} of type {EventType}",
+                    @event.EventId, @event.EventType);
+            }
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private async Task ProcessEventAsync(DomainEvent @event, CancellationToken cancellationToken)
+    {
+        // Handle specific event types
+        switch (@event)
+        {
+            case SagaCreatedEvent sagaCreated:
+                await HandleSagaCreatedAsync(sagaCreated);
+                break;
+
+            case SagaCompletedEvent sagaCompleted:
+                await HandleSagaCompletedAsync(sagaCompleted);
+                break;
+
+            case SagaFailedEvent sagaFailed:
+                await HandleSagaFailedAsync(sagaFailed);
+                break;
+
+            case CompensationStartedEvent compensationStarted:
+                await HandleCompensationStartedAsync(compensationStarted);
+                break;
+
+            case CompensationCompletedEvent compensationCompleted:
+                await HandleCompensationCompletedAsync(compensationCompleted);
+                break;
+
+            default:
+                _logger.LogDebug("Unhandled event type: {EventType}", @event.EventType);
+                break;
+        }
+    }
+
+    private async Task HandleSagaCreatedAsync(SagaCreatedEvent @event)
+    {
+        _logger.LogInformation(
+            "Processing SagaCreated event | SagaId: {SagaId}, Name: {SagaName}",
+            @event.SagaId, @event.SagaName);
+        await Task.CompletedTask;
+    }
+
+    private async Task HandleSagaCompletedAsync(SagaCompletedEvent @event)
+    {
+        _logger.LogInformation(
+            "Processing SagaCompleted event | SagaId: {SagaId}, Duration: {Duration}ms",
+            @event.SagaId, @event.DurationMs);
+        await Task.CompletedTask;
+    }
+
+    private async Task HandleSagaFailedAsync(SagaFailedEvent @event)
+    {
+        _logger.LogWarning(
+            "Processing SagaFailed event | SagaId: {SagaId}, Error: {Error}",
+            @event.SagaId, @event.ErrorMessage);
+        await Task.CompletedTask;
+    }
+
+    private async Task HandleCompensationStartedAsync(CompensationStartedEvent @event)
+    {
+        _logger.LogWarning(
+            "Processing CompensationStarted event | SagaId: {SagaId}, Strategy: {Strategy}",
+            @event.SagaId, @event.CompensationStrategy);
+        await Task.CompletedTask;
+    }
+
+    private async Task HandleCompensationCompletedAsync(CompensationCompletedEvent @event)
+    {
+        _logger.LogInformation(
+            "Processing CompensationCompleted event | SagaId: {SagaId}, Steps: {Steps}",
+            @event.SagaId, @event.CompensatedSteps);
+        await Task.CompletedTask;
+    }
+}
