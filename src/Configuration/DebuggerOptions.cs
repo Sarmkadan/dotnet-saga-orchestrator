b796604737781dade50072e395c36b255103576c
Vladyslav Zaiets <rutova2@gmail.com>
2021-11-09 08:39:26 +0200

    tighten guard clauses in DebuggerOptions
    mostly DebuggerOptions.cs, nothing exciting.

---
+++ src/Configuration/DebuggerOptions.cs
@@ -0,0 +1,202 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System;
+
+namespace SagaOrchestrator.Configuration;
+
+/// <summary>
+/// Configuration options for the distributed saga debugger.
+/// Can be loaded from <c>appsettings.json</c> under the
+/// <see cref="SectionName"/> key or configured programmatically via
+/// <see cref="DebuggerOptionsBuilder"/>.
+/// </summary>
+public sealed class DebuggerOptions
+{
+    /// <summary>Configuration section key used when binding from <c>appsettings.json</c>.</summary>
+    public const string SectionName = "SagaDebugger";
+
+    /// <summary>
+    /// Whether the debugger is active.  When <c>false</c> all operations become no-ops,
+    /// adding zero overhead to production paths.
+    /// Defaults to <c>false</c>.
+    /// </summary>
+    public bool IsEnabled { get; set; } = false;
+
+    /// <summary>
+    /// Maximum number of snapshots retained per saga in memory.
+    /// Oldest snapshots are evicted when the limit is reached.
+    /// Defaults to <c>50</c>.
+    /// </summary>
+    public int MaxSnapshotsPerSaga { get; set; } = 50;
+
+    /// <summary>
+    /// When <c>true</c>, a snapshot is automatically captured each time a step
+    /// transitions to <c>Executing</c> or <c>Completed</c>.
+    /// Defaults to <c>true</c>.
+    /// </summary>
+    public bool AutoCaptureOnStepTransition { get; set; } = true;
+
+    /// <summary>
+    /// When <c>true</c>, a snapshot is automatically captured when the saga
+    /// enters the compensation phase.
+    /// Defaults to <c>true</c>.
+    /// </summary>
+    public bool AutoCaptureOnCompensation { get; set; } = true;
+
+    /// <summary>
+    /// When <c>true</c>, a snapshot is automatically captured when the saga
+    /// reaches any terminal state (Completed, Failed, Compensated, TimedOut).
+    /// Defaults to <c>true</c>.
+    /// </summary>
+    public bool AutoCaptureOnTerminalState { get; set; } = true;
+
+    /// <summary>
+    /// Maximum number of breakpoints that can be registered per saga simultaneously.
+    /// Defaults to <c>20</c>.
+    /// </summary>
+    public int MaxBreakpointsPerSaga { get; set; } = 20;
+
+    /// <summary>
+    /// When <c>true</c>, step-level payload data is included in snapshots.
+    /// Set to <c>false</c> in environments with strict data-residency requirements.
+    /// Defaults to <c>true</c>.
+    /// </summary>
+    public bool IncludeStepPayloads { get; set; } = true;
+
+    /// <summary>
+    /// When <c>true</c>, saga metadata is included in snapshots.
+    /// Defaults to <c>true</c>.
+    /// </summary>
+    public bool IncludeSagaMetadata { get; set; } = true;
+
+    /// <summary>
+    /// When <c>true</c>, the time-travel (<c>TravelTo</c>) feature is available.
+    /// Disabling it prevents accidental state restoration in production.
+    /// Defaults to <c>false</c>.
+    /// </summary>
+    public bool EnableTimeTravel { get; set; } = false;
+}
+
+/// <summary>
+/// Fluent builder for constructing a validated <see cref="DebuggerOptions"/> instance.
+/// </summary>
+/// <example>
+/// <code>
+/// var opts = new DebuggerOptionsBuilder()
+///     .Enable()
+///     .WithMaxSnapshotsPerSaga(100)
+///     .WithAutoCapture(onStepTransition: true, onCompensation: true)
+///     .WithTimeTravel(enabled: true)
+///     .Build();
+/// </code>
+/// </example>
+public sealed class DebuggerOptionsBuilder
+{
+    private readonly DebuggerOptions _options = new();
+
+    /// <summary>
+    /// Enables or disables the debugger.
+    /// </summary>
+    public DebuggerOptionsBuilder Enable(bool enabled = true)
+    {
+        _options.IsEnabled = enabled;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the maximum number of snapshots retained per saga.
+    /// </summary>
+    /// <param name="max">Must be between 1 and 10 000.</param>
+    public DebuggerOptionsBuilder WithMaxSnapshotsPerSaga(int max)
+    {
+        if (max < 1 || max > 10_000)
+            throw new ArgumentOutOfRangeException(nameof(max), "Value must be between 1 and 10 000.");
+
+        _options.MaxSnapshotsPerSaga = max;
+        return this;
+    }
+
+    /// <summary>
+    /// Configures the automatic snapshot capture triggers.
+    /// </summary>
+    /// <param name="onStepTransition">Capture on each step start or completion.</param>
+    /// <param name="onCompensation">Capture when the saga enters compensation.</param>
+    /// <param name="onTerminalState">Capture when the saga reaches a terminal state.</param>
+    public DebuggerOptionsBuilder WithAutoCapture(
+        bool onStepTransition = true,
+        bool onCompensation   = true,
+        bool onTerminalState  = true)
+    {
+        _options.AutoCaptureOnStepTransition = onStepTransition;
+        _options.AutoCaptureOnCompensation   = onCompensation;
+        _options.AutoCaptureOnTerminalState  = onTerminalState;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the maximum number of simultaneous breakpoints per saga.
+    /// </summary>
+    /// <param name="max">Must be between 1 and 100.</param>
+    public DebuggerOptionsBuilder WithMaxBreakpointsPerSaga(int max)
+    {
+        if (max < 1 || max > 100)
+            throw new ArgumentOutOfRangeException(nameof(max), "Value must be between 1 and 100.");
+
+        _options.MaxBreakpointsPerSaga = max;
+        return this;
+    }
+
+    /// <summary>
+    /// Controls whether step payload and saga metadata are included in snapshots.
+    /// </summary>
+    /// <param name="includePayloads">Include step output data.</param>
+    /// <param name="includeMetadata">Include saga metadata dictionary.</param>
+    public DebuggerOptionsBuilder WithDataInclusion(
+        bool includePayloads = true,
+        bool includeMetadata = true)
+    {
+        _options.IncludeStepPayloads  = includePayloads;
+        _options.IncludeSagaMetadata  = includeMetadata;
+        return this;
+    }
+
+    /// <summary>
+    /// Enables or disables the time-travel (state restoration) feature.
+    /// </summary>
+    public DebuggerOptionsBuilder WithTimeTravel(bool enabled = true)
+    {
+        _options.EnableTimeTravel = enabled;
+        return this;
+    }
+
+    /// <summary>
+    /// Validates and returns the configured <see cref="DebuggerOptions"/> instance.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown when time-travel is enabled but the debugger itself is disabled, or when
+    /// <see cref="DebuggerOptions.MaxSnapshotsPerSaga"/> is zero.
+    /// </exception>
+    public DebuggerOptions Build()
+    {
+        Validate();
+        return _options;
+    }
+
+    private void Validate()
+    {
+        if (_options.EnableTimeTravel && !_options.IsEnabled)
+            throw new InvalidOperationException(
+                "Time-travel cannot be enabled while the debugger itself is disabled.");
+
+        if (_options.MaxSnapshotsPerSaga < 1)
+            throw new InvalidOperationException(
+                $"{nameof(DebuggerOptions.MaxSnapshotsPerSaga)} must be at least 1.");
+
+        if (_options.MaxBreakpointsPerSaga < 1)
+            throw new InvalidOperationException(
+                $"{nameof(DebuggerOptions.MaxBreakpointsPerSaga)} must be at least 1.");
+    }
+}
---
+++ src/Configuration/DebuggerServiceExtensions.cs
@@ -0,0 +1,114 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using SagaOrchestrator.Infrastructure.Debugging;
+
+namespace SagaOrchestrator.Configuration;
+
+/// <summary>
+/// Extension methods for registering the distributed saga debugger with the
+/// dependency injection container.
+/// </summary>
+public static class DebuggerServiceExtensions
+{
+    /// <summary>
+    /// Registers the <see cref="ISagaDebugger"/> service and its <see cref="DebuggerOptions"/>
+    /// configuration as singletons.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The debugger requires that <c>AddSagaOrchestrator()</c> has already been called so that
+    /// <c>ISagaRepository</c>, <c>ISagaStepRepository</c>, and <c>SagaEventPublisher</c> are
+    /// resolvable from the container.
+    /// </para>
+    /// <para>
+    /// Time-travel and auto-capture are opt-in; see <see cref="DebuggerOptions"/> for all flags.
+    /// In production, keep <see cref="DebuggerOptions.IsEnabled"/> as <c>false</c> (the default)
+    /// to eliminate any runtime overhead.
+    /// </para>
+    /// </remarks>
+    /// <param name="services">The <see cref="IServiceCollection"/> to configure.</param>
+    /// <param name="configure">
+    /// Optional delegate for customising <see cref="DebuggerOptions"/>.
+    /// When omitted the default options are used (debugger disabled).
+    /// </param>
+    /// <returns>The same <see cref="IServiceCollection"/> for method chaining.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="services"/> is <c>null</c>.
+    /// </exception>
+    /// <example>
+    /// <code>
+    /// // Development – full debugging with time-travel:
+    /// services.AddSagaOrchestrator()
+    ///         .AddSagaDebugger(opts =>
+    ///         {
+    ///             opts.IsEnabled              = true;
+    ///             opts.EnableTimeTravel       = true;
+    ///             opts.AutoCaptureOnStepTransition = true;
+    ///             opts.MaxSnapshotsPerSaga    = 100;
+    ///         });
+    ///
+    /// // Production – debugger disabled (default), zero overhead:
+    /// services.AddSagaOrchestrator()
+    ///         .AddSagaDebugger();
+    /// </code>
+    /// </example>
+    public static IServiceCollection AddSagaDebugger(
+        this IServiceCollection services,
+        Action<DebuggerOptions>? configure = null)
+    {
+        if (services is null)
+            throw new ArgumentNullException(nameof(services));
+
+        var options = new DebuggerOptions();
+        configure?.Invoke(options);
+
+        services.AddSingleton(options);
+        services.AddSingleton<ISagaDebugger, SagaDebuggerService>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers the <see cref="ISagaDebugger"/> service using a pre-built
+    /// <see cref="DebuggerOptions"/> instance produced by <see cref="DebuggerOptionsBuilder"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to configure.</param>
+    /// <param name="options">
+    /// A fully validated <see cref="DebuggerOptions"/> object, typically the output of
+    /// <see cref="DebuggerOptionsBuilder.Build"/>.
+    /// </param>
+    /// <returns>The same <see cref="IServiceCollection"/> for method chaining.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="services"/> or <paramref name="options"/> is <c>null</c>.
+    /// </exception>
+    /// <example>
+    /// <code>
+    /// var debuggerOptions = new DebuggerOptionsBuilder()
+    ///     .Enable()
+    ///     .WithMaxSnapshotsPerSaga(200)
+    ///     .WithAutoCapture(onStepTransition: true, onCompensation: true)
+    ///     .WithTimeTravel(enabled: true)
+    ///     .Build();
+    ///
+    /// services.AddSagaOrchestrator()
+    ///         .AddSagaDebugger(debuggerOptions);
+    /// </code>
+    /// </example>
+    public static IServiceCollection AddSagaDebugger(
+        this IServiceCollection services,
+        DebuggerOptions options)
+    {
+        if (services is null) throw new ArgumentNullException(nameof(services));
+        if (options  is null) throw new ArgumentNullException(nameof(options));
+
+        services.AddSingleton(options);
+        services.AddSingleton<ISagaDebugger, SagaDebuggerService>();
+
+        return services;
+    }
+}
