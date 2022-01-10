84c11a9f0367a310ed85d7ed050420d75adce4d6
Vladyslav Zaiets <rutova2@gmail.com>
2022-01-10 07:19:51 +0200

    extension methods on SagaException

--- src/Core/Exceptions/SagaExceptionExtensions.cs
+++ src/Core/Exceptions/SagaExceptionExtensions.cs
@@ -2,15 +2,45 @@ using System;
 
 namespace SagaOrchestrator.Core.Exceptions
 {
+    /// <summary>
+    /// Provides extension methods for <see cref="SagaException"/> to facilitate common exception handling scenarios.
+    /// </summary>
     public static class SagaExceptionExtensions
     {
-        public static bool IsSagaNotFound(this SagaException ex) =>
-            ex.ErrorCode == "SAGA_NOT_FOUND";
+        /// <summary>
+        /// Determines whether the exception represents a saga not found scenario.
+        /// </summary>
+        /// <param name="ex">The exception to check. Cannot be null.</param>
+        /// <returns>True if the error code is "SAGA_NOT_FOUND"; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="ex"/> is null.</exception>
+        public static bool IsSagaNotFound(this SagaException ex)
+        {
+            ArgumentNullException.ThrowIfNull(ex);
+            return ex.ErrorCode == "SAGA_NOT_FOUND";
+        }
 
-        public static bool IsSagaTimeout(this SagaException ex) =>
-            ex.ErrorCode == "SAGA_TIMEOUT";
+        /// <summary>
+        /// Determines whether the exception represents a saga timeout scenario.
+        /// </summary>
+        /// <param name="ex">The exception to check. Cannot be null.</param>
+        /// <returns>True if the error code is "SAGA_TIMEOUT"; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="ex"/> is null.</exception>
+        public static bool IsSagaTimeout(this SagaException ex)
+        {
+            ArgumentNullException.ThrowIfNull(ex);
+            return ex.ErrorCode == "SAGA_TIMEOUT";
+        }
 
-        public static string GetDetailedMessage(this SagaException ex) =>
-            $"Saga Id: {ex.SagaId}, Error Code: {ex.ErrorCode}, Message: {ex.Message}";
+        /// <summary>
+        /// Creates a detailed message string containing all relevant saga exception information.
+        /// </summary>
+        /// <param name="ex">The exception containing the saga information. Cannot be null.</param>
+        /// <returns>A formatted string with saga ID, error code, and message.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="ex"/> is null.</exception>
+        public static string GetDetailedMessage(this SagaException ex)
+        {
+            ArgumentNullException.ThrowIfNull(ex);
+            return $"Saga Id: {ex.SagaId}, Error Code: {ex.ErrorCode}, Message: {ex.Message}";
+        }
     }
 }
