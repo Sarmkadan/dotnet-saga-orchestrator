4dda98a0268bd30832d94865ad1abf9f7e829be9
Vladyslav Zaiets <rutova2@gmail.com>
2022-02-08 12:52:32 +0200

    Add SagaStepDefiintionJson json helpers

---
+++ src/Core/Domain/Models/SagaStepDefinitionJsonExtensions.cs
@@ -0,0 +1,82 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SagaOrchestrator.Core.Domain.Models;
+
+/// <summary>
+/// Provides JSON serialization and deserialization helpers for the <see cref="SagaStepDefinition"/> class.
+/// </summary>
+public static class SagaStepDefinitionJsonExtensions
+{
+    /// <summary>
+    /// Configured JSON serializer options with camelCase naming policy.
+    /// </summary>
+    private static readonly JsonSerializerOptions JsonOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="SagaStepDefinition"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The <see cref="SagaStepDefinition"/> instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>A JSON string representation of the <see cref="SagaStepDefinition"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this SagaStepDefinition value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = new JsonSerializerOptions(JsonOptions)
+        {
+            WriteIndented = indented
+        };
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="SagaStepDefinition"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized <see cref="SagaStepDefinition"/> instance, or null if deserialization fails.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="JsonException">Thrown when JSON deserialization fails.</exception>
+    public static SagaStepDefinition? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        return JsonSerializer.Deserialize<SagaStepDefinition>(json, JsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="SagaStepDefinition"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">The deserialized <see cref="SagaStepDefinition"/> instance, or null if deserialization fails.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJson(string json, out SagaStepDefinition? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<SagaStepDefinition>(json, JsonOptions);
+            return value is not null;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
