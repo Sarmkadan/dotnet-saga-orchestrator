f482ca529aa24510433aca2f0ad7df0a67ed4861
Vladyslav Zaiets <rutova2@gmail.com>
2022-02-09 20:52:06 +0200

    cleanup

---
+++ tests/dotnet-saga-orchestrator.Tests/RetryPolicyTestsJsonExtensions.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using SagaOrchestrator.Core.Utilities;
+
+namespace SagaOrchestrator.Tests;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for RetryPolicy.
+/// </summary>
+public static class RetryPolicyTestsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// Serializes a RetryPolicy instance to JSON.
+    /// </summary>
+    /// <param name="value">The RetryPolicy instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the RetryPolicy instance.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this RetryPolicy value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a RetryPolicy instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A RetryPolicy instance, or null if deserialization fails.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+    public static RetryPolicy? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        try
+        {
+            return JsonSerializer.Deserialize<RetryPolicy>(json, _jsonSerializerOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a RetryPolicy instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">The deserialized RetryPolicy instance, or null if deserialization fails.</param>
+    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+    public static bool TryFromJson(string json, out RetryPolicy? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return false;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<RetryPolicy>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
