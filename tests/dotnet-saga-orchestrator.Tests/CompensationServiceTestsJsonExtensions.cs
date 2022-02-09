1358ddf0f8f6fb08c976a43ef3a3dfc5656d1daf
Vladyslav Zaiets <rutova2@gmail.com>
2022-02-09 11:18:44 +0200

    feat: CompensationServiceTestsJson (de)serialization helpers

---
+++ tests/dotnet-saga-orchestrator.Tests/CompensationServiceTestsJsonExtensions.cs
@@ -0,0 +1,74 @@
+#nullable enable
+
+using System.Text.Json;
+
+namespace SagaOrchestrator.Tests;
+
+public static class CompensationServiceTestsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="CompensationServiceTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The value to serialize.</param>
+    /// <param name="indented">Whether to indent the JSON for readability.</param>
+    /// <returns>The JSON representation of the value.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this CompensationServiceTests value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        return JsonSerializer.Serialize(value, indented ? GetIndentedOptions() : _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="CompensationServiceTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized instance, or <see langword="null"/> if the JSON is empty.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="json"/> is <see langword="null"/>.</exception>
+    public static CompensationServiceTests? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        return json.Length == 0
+            ? null
+            : JsonSerializer.Deserialize<CompensationServiceTests>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="CompensationServiceTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance if successful.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    public static bool TryFromJson(string json, out CompensationServiceTests? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<CompensationServiceTests>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+
+    private static JsonSerializerOptions GetIndentedOptions()
+    {
+        var options = new JsonSerializerOptions(_jsonSerializerOptions)
+        {
+            WriteIndented = true,
+        };
+        return options;
+    }
+}
