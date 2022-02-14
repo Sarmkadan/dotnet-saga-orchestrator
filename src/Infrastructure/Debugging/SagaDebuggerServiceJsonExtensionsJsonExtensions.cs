0c0e70bdd93cc914d3988bfda37a1ce282e6c0b4
Vladyslav Zaiets <rutova2@gmail.com>
2022-02-14 15:10:35 +0200

    SagaDebuggerServiceJsonExtensionsJson (de)serialization helpers
    should be backwards compatible.

---
+++ src/Infrastructure/Debugging/SagaDebuggerServiceJsonExtensionsJsonExtensions.cs
@@ -0,0 +1,102 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SagaOrchestrator.Infrastructure.Debugging;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="SagaDebuggerService"/>.
+/// Enables easy JSON serialization and deserialization of saga debugger instances for
+/// debugging, logging, and transport scenarios.
+/// </summary>
+/// <remarks>
+/// This class complements <see cref="SagaDebuggerServiceJsonExtensions"/> by providing
+/// additional instance-based extension methods for <see cref="SagaDebuggerService"/>.
+/// </remarks>
+public static class SagaDebuggerServiceJsonExtensionsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = CreateJsonSerializerOptions();
+
+    private static JsonSerializerOptions CreateJsonSerializerOptions()
+    {
+        return new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = false,
+            ReferenceHandler = ReferenceHandler.IgnoreCycles,
+        };
+    }
+
+    /// <summary>
+    /// Serializes the <see cref="SagaDebuggerService"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The debugger service instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON representation of the debugger service.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this SagaDebuggerService value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = new JsonSerializerOptions(_jsonOptions)
+        {
+            WriteIndented = indented
+        };
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string into a <see cref="SagaDebuggerService"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized debugger service instance, or null if deserialization fails.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or whitespace.</exception>
+    public static SagaDebuggerService? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(json, nameof(json));
+
+        try
+        {
+            return JsonSerializer.Deserialize<SagaDebuggerService>(json, _jsonOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string into a <see cref="SagaDebuggerService"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized debugger service instance if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or whitespace.</exception>
+    public static bool TryFromJson(string json, out SagaDebuggerService? value)
+    {
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return false;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<SagaDebuggerService>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
