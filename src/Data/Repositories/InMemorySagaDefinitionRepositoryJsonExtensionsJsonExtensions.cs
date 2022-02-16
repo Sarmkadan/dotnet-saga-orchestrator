72b78475455ba3763738ec994c647e1d1ecb8f5d
Vladyslav Zaiets <rutova2@gmail.com>
2022-02-16 21:14:09 +0200

    add InMemorySagaDefinitionRepositoryJsonExtensionsJson json helpers

---
+++ src/Data/Repositories/InMemorySagaDefinitionRepositoryJsonExtensionsJsonExtensions.cs
@@ -0,0 +1,89 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System;
+using System.Text.Json;
+
+namespace SagaOrchestrator.Data.Repositories;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for InMemorySagaDefinitionRepository.
+/// </summary>
+public static class InMemorySagaDefinitionRepositoryJsonExtensionsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes the InMemorySagaDefinitionRepository to a JSON string.
+    /// </summary>
+    /// <param name="value">The repository instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the repository.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this InMemorySagaDefinitionRepository value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to an InMemorySagaDefinitionRepository instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized repository instance, or null if the JSON is null, empty, or invalid.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    public static InMemorySagaDefinitionRepository? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+            return null;
+
+        try
+        {
+            return JsonSerializer.Deserialize<InMemorySagaDefinitionRepository>(json, _jsonSerializerOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to an InMemorySagaDefinitionRepository instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">The deserialized repository instance, or null if deserialization fails.</param>
+    /// <returns>True if deserialization succeeded; false otherwise.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    public static bool TryFromJson(string json, out InMemorySagaDefinitionRepository? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+            return false;
+
+        try
+        {
+            value = JsonSerializer.Deserialize<InMemorySagaDefinitionRepository>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
