79f3c8181c56bd868feba51caa02e3a4c0e1c6e6
Vladyslav Zaiets <rutova2@gmail.com>
2022-02-16 08:21:55 +0200

    Tidy up ValidationExceptionJsonExtensionsJson json handling

---
+++ src/Core/Exceptions/ValidationExceptionJsonExtensionsJsonExtensions.cs
@@ -0,0 +1,84 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// =============================================================================
+
+using System;
+using System.Text.Json;
+using SagaOrchestrator.Core.Exceptions;
+
+namespace SagaOrchestrator.Core.Exceptions;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="ValidationException"/>.
+/// </summary>
+public static class ValidationExceptionJsonExtensionsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="ValidationException"/> to a JSON string.
+    /// </summary>
+    /// <param name="value">The exception to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
+    /// <returns>A JSON string representation of the exception.</returns>
+    public static string ToJson(this ValidationException value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a <see cref="ValidationException"/> from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+    /// <returns>The deserialized exception, or <see langword="null"/> if the JSON is <see langword="null"/> or empty.</returns>
+    public static ValidationException? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        return string.IsNullOrEmpty(json)
+            ? null
+            : JsonSerializer.Deserialize<ValidationException>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a <see cref="ValidationException"/> from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">The deserialized exception if successful, otherwise null.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    public static bool TryFromJson(string json, out ValidationException? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        value = null;
+
+        if (string.IsNullOrEmpty(json))
+        {
+            return true;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<ValidationException>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
