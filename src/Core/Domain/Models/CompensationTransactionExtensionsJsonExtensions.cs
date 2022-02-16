f3dec073766377c1b818d5d42cde5a35689a19d8
Vladyslav Zaiets <rutova2@gmail.com>
2022-02-16 12:45:31 +0200

    serialize CompensationTransactionExtensionsJson properly.

---
+++ src/Core/Domain/Models/CompensationTransactionExtensionsJsonExtensions.cs
@@ -0,0 +1,83 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SagaOrchestrator.Core.Domain.Models;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="CompensationTransaction"/>.
+/// </summary>
+public static class CompensationTransactionExtensionsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        WriteIndented = false,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="CompensationTransaction"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The compensation transaction to serialize</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability</param>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is null</exception>
+    /// <returns>A JSON string representation of the compensation transaction</returns>
+    public static string ToJson(this CompensationTransaction value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="CompensationTransaction"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null</exception>
+    /// <exception cref="JsonException">The JSON is invalid or cannot be deserialized</exception>
+    /// <returns>The deserialized <see cref="CompensationTransaction"/> instance</returns>
+    public static CompensationTransaction? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        return JsonSerializer.Deserialize<CompensationTransaction>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="CompensationTransaction"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <param name="value">Receives the deserialized value if successful</param>
+    /// <returns>True if deserialization succeeded; otherwise false</returns>
+    public static bool TryFromJson(string json, out CompensationTransaction? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<CompensationTransaction>(json, _jsonSerializerOptions);
+            return value is not null;
+        }
+        catch (JsonException)
+        {
+            value = default;
+            return false;
+        }
+    }
+}
