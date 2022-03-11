565a6afdb8b71fbab3b593f6c51ff3881e6c3c4a
Vladyslav Zaiets <rutova2@gmail.com>
2022-03-11 11:35:46 +0200

    json serialization for TimelineEntryJson

---
+++ src/Infrastructure/Debugging/TimelineEntryJsonExtensions.cs
@@ -0,0 +1,86 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SagaOrchestrator.Infrastructure.Debugging;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="TimelineEntry"/>.
+/// </summary>
+public static class TimelineEntryJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="TimelineEntry"/> to a JSON string.
+    /// </summary>
+    /// <param name="value">The timeline entry to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON representation of the timeline entry.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this TimelineEntry value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        if (indented)
+        {
+            _jsonSerializerOptions.WriteIndented = true;
+        }
+
+        return JsonSerializer.Serialize(value, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string into a <see cref="TimelineEntry"/>.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized timeline entry, or null if deserialization fails.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    public static TimelineEntry? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        try
+        {
+            return JsonSerializer.Deserialize<TimelineEntry>(json, _jsonSerializerOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string into a <see cref="TimelineEntry"/>.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized timeline entry if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    public static bool TryFromJson(string json, out TimelineEntry? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<TimelineEntry>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
