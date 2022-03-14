228820cbbda90024923886a8c1f4268b0f3d5d76
Vladyslav Zaiets <rutova2@gmail.com>
2022-03-14 13:09:18 +0200

    refactor: nits

--- tests/dotnet-saga-orchestrator.Tests/TimeoutPolicyTests.cs
+++ tests/dotnet-saga-orchestrator.Tests/TimeoutPolicyTests.cs
@@ -6,30 +6,41 @@ namespace SagaOrchestrator.Tests;
 
 /// <summary>
 /// Test data transfer object for timeout policy testing scenarios.
+/// Provides predefined timeout configurations and conversion to <see cref="TimeoutPolicy"/>.
 /// </summary>
 public record TimeoutPolicyTests(
-int TimeoutSeconds,
-bool IsRelaxed
+    /// <summary>
+    /// Gets the timeout duration in seconds.
+    /// </summary>
+    int TimeoutSeconds,
+
+    /// <summary>
+    /// Gets a value indicating whether the timeout policy is relaxed.
+    /// </summary>
+    bool IsRelaxed
 )
 {
-/// <summary>
-/// Creates a standard timeout policy test data (60 seconds, not relaxed).
-/// </summary>
-public static TimeoutPolicyTests Standard => new(60, false);
+    /// <summary>
+    /// Gets a standard timeout policy test data configuration (60 seconds, not relaxed).
+    /// </summary>
+    public static TimeoutPolicyTests Standard => new(60, false);
 
-/// <summary>
-/// Creates a lenient timeout policy test data (300 seconds, relaxed).
-/// </summary>
-public static TimeoutPolicyTests Lenient => new(300, true);
+    /// <summary>
+    /// Gets a lenient timeout policy test data configuration (300 seconds, relaxed).
+    /// </summary>
+    public static TimeoutPolicyTests Lenient => new(300, true);
 
-/// <summary>
-/// Creates a strict timeout policy test data (10 seconds, not relaxed).
-/// </summary>
-public static TimeoutPolicyTests Strict => new(10, false);
+    /// <summary>
+    /// Gets a strict timeout policy test data configuration (10 seconds, not relaxed).
+    /// </summary>
+    public static TimeoutPolicyTests Strict => new(10, false);
 
-/// <summary>
-/// Converts the timeout policy test data to a TimeoutPolicy instance.
-/// </summary>
-/// <returns>A TimeoutPolicy instance.</returns>
-public TimeoutPolicy ToPolicy() => new(this.TimeoutSeconds);
+    /// <summary>
+    /// Converts the timeout policy test data to a <see cref="TimeoutPolicy"/> instance.
+    /// </summary>
+    /// <returns>
+    /// A new <see cref="TimeoutPolicy"/> instance initialized with the <see cref="TimeoutSeconds"/>.
+    /// The <see cref="IsRelaxed"/> property is not used in this conversion.
+    /// </returns>
+    public TimeoutPolicy ToPolicy() => new(this.TimeoutSeconds);
 }
