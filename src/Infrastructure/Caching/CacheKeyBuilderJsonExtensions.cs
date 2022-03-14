abc162bf3bc3c92a77b2aec468da840d949a5ba2
Vladyslav Zaiets <rutova2@gmail.com>
2022-03-14 09:24:41 +0200

    feat: imnor

--- src/Infrastructure/Caching/CacheKeyBuilderJsonExtensions.cs
+++ src/Infrastructure/Caching/CacheKeyBuilderJsonExtensions.cs
@@ -4,6 +4,8 @@
 // CTO & Software Architect
 // =============================================================================
 
+using System.Text.Json;
+
 namespace SagaOrchestrator.Infrastructure.Caching;
 
 /// <summary>
@@ -18,29 +20,31 @@ public static class CacheKeyBuilderJsonExtensions
     };
 
     /// <summary>
-    /// Converts a <see cref="CacheKeyBuilder"/> to a JSON string.
+    /// Converts a cache key to a JSON string.
     /// </summary>
-    /// <param name="value">The <see cref="CacheKeyBuilder"/> to convert.</param>
+    /// <param name="key">The cache key to convert.</param>
     /// <param name="indented">Whether the JSON should be formatted with indentation.</param>
-    /// <returns>A JSON string representation of the <see cref="CacheKeyBuilder"/>.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
-    public static string ToJson(this CacheKeyBuilder value, bool indented = false)
+    /// <returns>A JSON string representation of the cache key.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="key"/> is null.</exception>
+    public static string ToJson(this string key, bool indented = false)
     {
-        ArgumentNullException.ThrowIfNull(value);
+        ArgumentNullException.ThrowIfNull(key);
 
         var options = indented
             ? new JsonSerializerOptions(JsonOptions) { WriteIndented = true }
             : JsonOptions;
 
-        return JsonSerializer.Serialize(value, options);
+        var cacheKey = new { Key = key };
+
+        return JsonSerializer.Serialize(cacheKey, options);
     }
 
     /// <summary>
-    /// Deserializes a JSON string to a <see cref="CacheKeyBuilder"/>.
+    /// Deserializes a JSON string to a cache key.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>A deserialized <see cref="CacheKeyBuilder"/> or null if the JSON is invalid.</returns>
-    public static CacheKeyBuilder? FromJson(string json)
+    /// <returns>A deserialized cache key or null if the JSON is invalid.</returns>
+    public static string? FromJson(string json)
     {
         if (string.IsNullOrEmpty(json))
         {
@@ -49,7 +53,9 @@ public static class CacheKeyBuilderJsonExtensions
 
         try
         {
-            return JsonSerializer.Deserialize<CacheKeyBuilder>(json, JsonOptions);
+            var cacheKey = JsonSerializer.Deserialize<CacheKey>(json, JsonOptions);
+
+            return cacheKey?.Key;
         }
         catch (JsonException)
         {
@@ -58,22 +64,32 @@ public static class CacheKeyBuilderJsonExtensions
     }
 
     /// <summary>
-    /// Tries to deserialize a JSON string to a <see cref="CacheKeyBuilder"/>.
+    /// Tries to deserialize a JSON string to a cache key.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">The deserialized <see cref="CacheKeyBuilder"/> or null if the JSON is invalid.</param>
+    /// <param name="key">The deserialized cache key or null if the JSON is invalid.</param>
     /// <returns>True if the JSON was successfully deserialized; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out CacheKeyBuilder? value)
+    public static bool TryFromJson(string json, out string? key)
     {
         try
         {
-            value = FromJson(json);
-            return value != null;
+            key = FromJson(json);
+            return key != null;
         }
         catch (JsonException)
         {
-            value = null;
+            key = null;
             return false;
         }
+    }
+}
+
+public class CacheKey
+{
+    public string Key { get; set; }
+
+    public CacheKey(string key)
+    {
+        Key = key;
     }
 }
