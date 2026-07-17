4b481d78cec3e4a2f6c46b3e3250d77c80848363
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    refactor: cleanup in CompensationServiceJsonExtensions

--- src/Application/Services/CompensationServiceJsonExtensions.cs
+++ src/Application/Services/CompensationServiceJsonExtensions.cs
@@ -7,6 +7,9 @@ namespace SagaOrchestrator.Application.Services;
 /// <summary>
 /// Provides JSON serialization and deserialization helpers for the <see cref="CompensationService"/> class.
 /// </summary>
+/// <remarks>
+/// This class cannot be inherited.
+/// </remarks>
 public static class CompensationServiceJsonExtensions
 {
     /// <summary>
@@ -25,16 +28,8 @@ public static class CompensationServiceJsonExtensions
     /// <param name="indented">Whether to format the JSON with indentation.</param>
     /// <returns>A JSON string representation of the <see cref="CompensationService"/>.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this CompensationService value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-
-        var options = new JsonSerializerOptions(JsonOptions)
-        {
-            WriteIndented = indented
-        };
-        return JsonSerializer.Serialize(value, options);
-    }
+    public static string ToJson(this CompensationService value, bool indented = false) =>
+        JsonSerializer.Serialize(value, new JsonSerializerOptions(JsonOptions) { WriteIndented = indented });
 
     /// <summary>
     /// Deserializes a JSON string to a <see cref="CompensationService"/> instance.
@@ -42,7 +37,7 @@ public static class CompensationServiceJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <returns>The deserialized <see cref="CompensationService"/> instance, or null if deserialization fails.</returns>
     /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
-    /// <exception cref="JsonException">Thrown when JSON deserialization fails.</exception>
+    /// <exception cref="JsonException">Thrown when <paramref name="json"/> is not valid JSON or cannot be deserialized to <see cref="CompensationService"/>.</exception>
     public static CompensationService? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
@@ -56,9 +51,11 @@ public static class CompensationServiceJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">The deserialized <see cref="CompensationService"/> instance, or null if deserialization fails.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty.</exception>
     public static bool TryFromJson(string json, out CompensationService? value)
     {
+        ArgumentNullException.ThrowIfNull(json);
         ArgumentException.ThrowIfNullOrEmpty(json);
 
         try
