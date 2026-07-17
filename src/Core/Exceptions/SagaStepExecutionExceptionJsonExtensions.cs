3b3b6073c87ee9fbb972af1e91d6639c989baaf6
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 06:55:11 +0300

    document SagaStepExecutionExceptionJson public api

--- src/Core/Exceptions/SagaStepExecutionExceptionJsonExtensions.cs
+++ src/Core/Exceptions/SagaStepExecutionExceptionJsonExtensions.cs
@@ -4,6 +4,9 @@ using System.Text.Json.Serialization;
 
 namespace SagaOrchestrator.Core.Exceptions;
 
+/// <summary>
+/// Provides extension methods for serializing and deserializing <see cref="SagaStepExecutionException"/> instances to and from JSON.
+/// </summary>
 public static class SagaStepExecutionExceptionJsonExtensions
 {
     private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
@@ -12,6 +15,12 @@ public static class SagaStepExecutionExceptionJsonExtensions
         WriteIndented = false
     };
 
+    /// <summary>
+    /// Serializes the <paramref name="value"/> to a JSON string, optionally with indentation.
+    /// </summary>
+    /// <param name="value">The <see cref="SagaStepExecutionException"/> instance to serialize.</param>
+    /// <param name="indented">Whether to include indentation in the JSON output.</param>
+    /// <returns>The JSON representation of the <paramref name="value"/>.</returns>
     public static string ToJson(this SagaStepExecutionException value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -24,6 +33,11 @@ public static class SagaStepExecutionExceptionJsonExtensions
         return JsonSerializer.Serialize(value, _jsonSerializerOptions);
     }
 
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="SagaStepExecutionException"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized <see cref="SagaStepExecutionException"/> instance, or <c>null</c> if deserialization fails.</returns>
     public static SagaStepExecutionException? FromJson(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
@@ -38,6 +52,12 @@ public static class SagaStepExecutionExceptionJsonExtensions
         }
     }
 
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="SagaStepExecutionException"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">The deserialized <see cref="SagaStepExecutionException"/> instance, or <c>null</c> if deserialization fails.</param>
+    /// <returns><c>true</c> if deserialization is successful, <c>false</c> otherwise.</returns>
     public static bool TryFromJson(string json, out SagaStepExecutionException? value)
     {
         ArgumentNullException.ThrowIfNull(json);
