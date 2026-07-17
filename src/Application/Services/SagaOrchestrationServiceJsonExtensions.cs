fd2e7297e0b2d50077be3635faeec6d365212b16
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Unbreak SagaOrchestrationServiceJsonExtensions
    no behaviour change intended.

--- src/Application/Services/SagaOrchestrationServiceJsonExtensions.cs
+++ src/Application/Services/SagaOrchestrationServiceJsonExtensions.cs
@@ -2,31 +2,31 @@
 
 using System;
 using System.Text.Json;
-using System.Text.Json.Serialization.Metadata;
+using System.Text.Json.Serialization;
+using SagaOrchestrator.Core.Domain.Models;
 
 namespace SagaOrchestrator.Application.Services;
 
 /// <summary>
-/// Provides System.Text.Json serialization extensions for <see cref="SagaOrchestrationService"/>.
+/// Provides System.Text.Json serialization extensions for <see cref="Saga"/>.
 /// </summary>
 public static class SagaOrchestrationServiceJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         WriteIndented = false,
         PropertyNameCaseInsensitive = true,
-        TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
     };
 
     /// <summary>
-    /// Serializes the <see cref="SagaOrchestrationService"/> instance to a JSON string.
+    /// Serializes the <see cref="Saga"/> instance to a JSON string.
     /// </summary>
-    /// <param name="value">The service instance to serialize.</param>
+    /// <param name="value">The saga instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation.</param>
     /// <exception cref="ArgumentNullException"><paramref name="value"/> is null.</exception>
-    /// <returns>A JSON string representation of the service.</returns>
-    public static string ToJson(this SagaOrchestrationService value, bool indented = false)
+    /// <returns>A JSON string representation of the saga.</returns>
+    public static string ToJson(this Saga value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
 
@@ -41,18 +41,18 @@ public static class SagaOrchestrationServiceJsonExtensions
     }
 
     /// <summary>
-    /// Deserializes a JSON string to a <see cref="SagaOrchestrationService"/> instance.
+    /// Deserializes a JSON string to a <see cref="Saga"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <exception cref="ArgumentException"><paramref name="json"/> is null or whitespace.</exception>
-    /// <returns>The deserialized service instance, or null if the JSON is invalid.</returns>
-    public static SagaOrchestrationService? FromJson(string json)
+    /// <returns>The deserialized saga instance, or null if the JSON is invalid.</returns>
+    public static Saga? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(json);
 
         try
         {
-            return JsonSerializer.Deserialize<SagaOrchestrationService>(json, _jsonSerializerOptions);
+            return JsonSerializer.Deserialize<Saga>(json, _jsonSerializerOptions);
         }
         catch (JsonException)
         {
@@ -61,13 +61,13 @@ public static class SagaOrchestrationServiceJsonExtensions
     }
 
     /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="SagaOrchestrationService"/> instance.
+    /// Attempts to deserialize a JSON string to a <see cref="Saga"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">The deserialized service instance, or null if deserialization failed.</param>
+    /// <param name="value">The deserialized saga instance, or null if deserialization failed.</param>
     /// <exception cref="ArgumentException"><paramref name="json"/> is null or whitespace.</exception>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out SagaOrchestrationService? value)
+    public static bool TryFromJson(string json, out Saga? value)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(json);
 
@@ -75,7 +75,7 @@ public static class SagaOrchestrationServiceJsonExtensions
 
         try
         {
-            value = JsonSerializer.Deserialize<SagaOrchestrationService>(json, _jsonSerializerOptions);
+            value = JsonSerializer.Deserialize<Saga>(json, _jsonSerializerOptions);
             return true;
         }
         catch (JsonException)
--- src/Application/Services/SagaOrchestrationServiceJsonExtensionsJsonExtensions.cs
+++ src/Application/Services/SagaOrchestrationServiceJsonExtensionsJsonExtensions.cs
@@ -2,11 +2,13 @@
 
 using System;
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using SagaOrchestrator.Core.Domain.Models;
 
 namespace SagaOrchestrator.Application.Services;
 
 /// <summary>
-/// Provides System.Text.Json serialization extensions for <see cref="SagaOrchestrationService"/>.
+/// Provides System.Text.Json serialization extensions for <see cref="Saga"/>.
 /// </summary>
 public static class SagaOrchestrationServiceJsonExtensionsJsonExtensions
 {
@@ -18,13 +20,13 @@ public static class SagaOrchestrationServiceJsonExtensionsJsonExtensions
     };
 
     /// <summary>
-    /// Serializes the <see cref="SagaOrchestrationService"/> instance to a JSON string.
+    /// Serializes the <see cref="Saga"/> instance to a JSON string.
     /// </summary>
-    /// <param name="value">The service instance to serialize.</param>
+    /// <param name="value">The saga instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation.</param>
     /// <exception cref="ArgumentNullException"><paramref name="value"/> is null.</exception>
-    /// <returns>A JSON string representation of the service.</returns>
-    public static string ToJson(this SagaOrchestrationService value, bool indented = false)
+    /// <returns>A JSON string representation of the saga.</returns>
+    public static string ToJson(this Saga value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
 
@@ -39,18 +41,18 @@ public static class SagaOrchestrationServiceJsonExtensionsJsonExtensions
     }
 
     /// <summary>
-    /// Deserializes a JSON string to a <see cref="SagaOrchestrationService"/> instance.
+    /// Deserializes a JSON string to a <see cref="Saga"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <exception cref="ArgumentException"><paramref name="json"/> is null or whitespace.</exception>
-    /// <returns>The deserialized service instance, or null if the JSON is invalid.</returns>
-    public static SagaOrchestrationService? FromJson(string json)
+    /// <returns>The deserialized saga instance, or null if the JSON is invalid.</returns>
+    public static Saga? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(json);
 
         try
         {
-            return JsonSerializer.Deserialize<SagaOrchestrationService>(json, _jsonSerializerOptions);
+            return JsonSerializer.Deserialize<Saga>(json, _jsonSerializerOptions);
         }
         catch (JsonException)
         {
@@ -59,13 +61,13 @@ public static class SagaOrchestrationServiceJsonExtensionsJsonExtensions
     }
 
     /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="SagaOrchestrationService"/> instance.
+    /// Attempts to deserialize a JSON string to a <see cref="Saga"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">The deserialized service instance, or null if deserialization failed.</param>
+    /// <param name="value">The deserialized saga instance, or null if deserialization failed.</param>
     /// <exception cref="ArgumentException"><paramref name="json"/> is null or whitespace.</exception>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out SagaOrchestrationService? value)
+    public static bool TryFromJson(string json, out Saga? value)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(json);
 
@@ -73,7 +75,7 @@ public static class SagaOrchestrationServiceJsonExtensionsJsonExtensions
 
         try
         {
-            value = JsonSerializer.Deserialize<SagaOrchestrationService>(json, _jsonSerializerOptions);
+            value = JsonSerializer.Deserialize<Saga>(json, _jsonSerializerOptions);
             return true;
         }
         catch (JsonException)
