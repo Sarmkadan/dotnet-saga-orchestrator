5a99de593fc3ae9523e1ff1574cd30d8f6058d37
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-28 21:30:00 +0300

    fix: build fix

---
+++ src/Application/Services/SagaDiagnosticsService.cs
@@ -0,0 +1,12 @@
+using SagaOrchestrator.Services;
+
+namespace SagaOrchestrator.Services
+{
+    public class SagaDiagnosticsService
+    {
+        public async Task FindStuckSagasAsync(TimeSpan threshold)
+        {
+            // implement me
+        }
+    }
+}
