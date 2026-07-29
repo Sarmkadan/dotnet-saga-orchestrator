e7f785f43d6cd37f01e27a9ebb60a2abc29a78c6
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 21:30:00 +0300

    tweak CollectionExtensionsTests
    should be backwards compatible.

---
+++ tests/dotnet-saga-orchestrator.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,22 @@
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace dotnet_saga_orchestrator.Tests
+{
+    public class CollectionExtensionsTests
+    {
+        [Fact]
+        public void EmptyListTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EmptyListTest2()
+        {
+            // Test implementation here
+        }
+    }
+}
