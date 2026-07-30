5a92021157a62aa5b60572c462c731ce32194a10
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 21:30:00 +0300

    chore(dotnet-saga-orchestrator.tests): SagaExtensionsTests.cs

---
+++ tests/dotnet-saga-orchestrator.Tests/SagaExtensionsTests.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Moq;
+using System.Threading;
+using System;
+
+namespace dotnet_saga_orchestrator.Tests
+{
+    public class SagaExtensionsTests
+    {
+        [Fact]
+        public void HappyPath_IsCompleted()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_IsCompleted_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_IsCompleted_InvalidInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_IsTerminal()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_IsTerminal_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_IsTerminal_InvalidInput()
+        {
+            // Test implementation here
+        }
+    }
+}
