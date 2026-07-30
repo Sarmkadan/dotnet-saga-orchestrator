713b687e427f3cad2b8689727600fca2b9ae3642
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 11:21:51 +0300

    CollectionExtensionsJsonExtensionsTests changes

---
+++ tests/dotnet-saga-orchestrator.Tests/CollectionExtensionsJsonExtensionsTests.cs
@@ -0,0 +1,48 @@
+using Xunit;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+
+namespace dotnet_saga_orchestrator.Tests
+{
+    public class CollectionExtensionsJsonExtensionsTests
+    {
+        [Fact]
+        public async Task TestToJson_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestFromJson_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestTryFromJson_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestToJson_EmptyCollection()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestFromJson_EmptyCollection()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestTryFromJson_EmptyCollection()
+        {
+            // Test implementation here
+        }
+    }
+}
