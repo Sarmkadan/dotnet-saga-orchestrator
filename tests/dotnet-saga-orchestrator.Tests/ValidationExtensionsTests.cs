ffdc5036b403ee62f60a7c53768cf4cbfc80c6cf
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 07:12:02 +0300

    chore(dotnet-saga-orchestrator.tests): update ValidationExtensionsTests
    mostly ValidationExtensionsTests.cs, nothing exciting.

---
+++ tests/dotnet-saga-orchestrator.Tests/ValidationExtensionsTests.cs
@@ -0,0 +1,67 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dotnet_saga_orchestrator.Tests
+{
+    public class ValidationExtensionsTests
+    {
+        [Fact]
+        public void NotNullTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void NotNullOrEmptyTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void NotNullOrWhiteSpaceTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void InRangeTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GreaterThanTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GreaterThanOrEqualTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GreaterThanZeroTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void InRangeDecimalTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void NotEmptyTest()
+        {
+            // Test implementation here
+        }
+    }
+}
