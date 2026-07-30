230fca7346a7df1a4e0c1c72e9c1a62cb87d6502
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 17:37:41 +0300

    StringExtensionsValidationTests changes

---
+++ tests/dotnet-saga-orchestrator.Tests/StringExtensionsValidationTests.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Moq;
+using System;
+
+namespace dotnet_saga_orchestrator.Tests
+{
+    public class StringExtensionsValidationTests
+    {
+        [Fact]
+        public void Validate_Happy_PATH_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsValid_HAPPY_PATH_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EnsureValid_HAPPY_PATH_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void Validate_NULL_INPUT_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsValid_NULL_INPUT_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EnsureValid_NULL_INPUT_Test()
+        {
+            // Test implementation here
+        }
+    }
+}
