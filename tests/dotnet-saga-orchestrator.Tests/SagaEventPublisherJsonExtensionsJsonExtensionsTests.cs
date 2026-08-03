293bee3f2df97905a754c258de81d3912626419e
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-03 01:07:43 +0000

    chore: apply workspace updates

---
+++ tests/dotnet-saga-orchestrator.Tests/SagaEventPublisherJsonExtensionsJsonExtensionsTests.cs
@@ -0,0 +1,8 @@
+using Xunit;
+namespace dotnet_saga_orchestrator.Tests
+{
+    public class SagaEventPublisherJsonExtensionsJsonExtensionsTests
+    {
+        // Test methods will go here
+    }
+}
