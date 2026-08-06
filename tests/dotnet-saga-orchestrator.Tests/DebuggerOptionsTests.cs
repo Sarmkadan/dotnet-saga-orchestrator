e3322ba6b0260bd41e70f0cf1244aa4f0e39c89c
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-06 15:12:07 +0000

    chore: apply workspace updates

---
+++ tests/dotnet-saga-orchestrator.Tests/DebuggerOptionsTests.cs
@@ -0,0 +1,68 @@
+using Xunit;
+using dotnet_saga_orchestrator;
+
+namespace dotnet_saga_orchestrator.Tests
+{
+    public class DebuggerOptionsTests
+    {
+        [Fact]
+        public void TestIsEnabled()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TestMaxSnapshotsPerSaga()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TestAutoCaptureOnStepTransition()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TestAutoCaptureOnCompensation()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TestAutoCaptureOnTerminalState()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TestMaxBreakpointsPerSaga()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TestIncludeStepPayloads()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TestIncludeSagaMetadata()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TestEnableTimeTravel()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TestEnable()
+        {
+            // Test implementation here
+        }
+    }
+}
